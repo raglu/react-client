272038793bcf32b1084a16f2fe950bca380e265a
raglu16 <raglu16@student.sdu.dk>
2019-01-28 23:35:51 +0100

    Revert "Removed the react logo animation"
    This reverts commit 52d00b1d834a9b5248170d25cdc8388b41e0f290.

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import './homepage.css';
 
 class Homepage extends Component {
   render() {
     return (
       <div className="App">
-        <h1>This is a heading</h1>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
